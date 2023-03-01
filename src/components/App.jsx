1d000a0aa3f0f9c27bd2746de87ac58cb2a5f2d9
Cin💜 Lo <cinlos3382@gmail.com>
2023-03-01 10:42:05 -0300

    modif en el index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import '../styles/global.scss'
 
 const App = () => {
         return (
-         <h1>Holaa mundoooooo</h1>       
+         <h1>Holaa mundoooooos</h1>       
         )
 }
 export default App;
