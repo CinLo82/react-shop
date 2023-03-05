451d9f3618824c135d89260ab744c2716c6521cb
Cin💜 Lo <cinlos3382@gmail.com>
2023-03-05 10:01:00 -0300

    useEffect, traemos products de una API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import ProductItem from "components/ProductItem";
 import 'styles/ProductList.scss';
+import axios from "axios";
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await axios.get(API);
+			setProducts(response.data);
+		}
+		fetchData();
+	}, [])
+
     return(
         <section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product =>(
+					<ProductItem />
+				))}
 			</div>
 		</section>
     );
